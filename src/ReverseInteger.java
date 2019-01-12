039d13aa911ff489e1a6583cbea68fe5e48a6a72
zhengpeifeng <zhengpeifeng@leyaoyao.com>
2019-01-12 13:17:11 +0800

    :pizza:

--- src/ReverseInteger.java
+++ src/ReverseInteger.java
@@ -6,15 +6,13 @@ public class ReverseInteger {
     public static void main(String[] args) {
 
         boolean negative = false;
-        Integer test = 1534236469;
-        System.out.println(Integer.MAX_VALUE);
-        //1534236469
+        Integer test = 534236469;
+        Long start = System.currentTimeMillis();
+        //System.out.println(Integer.MAX_VALUE);
+        //System.out.println(reverse(test));
         System.out.println(getOne(test, 0));
+        System.out.println(System.currentTimeMillis() - start );
 
-    }
-
-    public int reverse(int x) {
-        return getOne(x, 0);
     }
 
     public static int getOne(Integer a, Integer res){ //尾递归版本
@@ -26,6 +24,18 @@ public class ReverseInteger {
             return getOne((a / 10), res) ;
         }
         return res;
+    }
+
+    public static int reverse(int x) {
+        int rev = 0;
+        while (x != 0) {
+            int pop = x % 10;
+            x /= 10;
+            if (rev > Integer.MAX_VALUE/10 || (rev == Integer.MAX_VALUE / 10 && pop > 7)) return 0;
+            if (rev < Integer.MIN_VALUE/10 || (rev == Integer.MIN_VALUE / 10 && pop < -8)) return 0;
+            rev = rev * 10 + pop;
+        }
+        return rev;
     }
 
 }
